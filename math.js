7b326bbf4fdbf25cf164c9ce6ca9911425c2139f
FayazCodeCraft <fayaz.m@codecraft.co.in>
2023-08-08 15:20:22 +0530

    feat: added add function

--- math.js
+++ math.js
@@ -1,1 +1,12 @@
 //Math Module
+//math module gti
+/**
+ * Adds two numbers a and b
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the result
+ */
+function add(a,b)
+{
+    return a+b;
+}
